25062d43f9e9a7018ddd08991c538f123720c7cc
Owen Rodley <Strigoides@gmail.com>
2013-12-06 19:45:09 +1300

    Add subcommands

--- main.go
+++ main.go
@@ -5,10 +5,34 @@ import (
 	"os"
 )
 
+var commands = []cli.Command{
+	{
+		Name:      "extract",
+		ShortName: "x",
+		Usage:     "extract files from the archive",
+	},
+	{
+		Name:      "make",
+		ShortName: "m",
+		Usage:     "create a new archive",
+	},
+	{
+		Name:      "list",
+		ShortName: "ls",
+		Usage:     "list the contents of an archive",
+	},
+	{
+		Name:      "add",
+		ShortName: "a",
+		Usage:     "add files to an existing archive",
+	},
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "farc"
 	app.Usage = "file archiver & compressor"
+	app.Commands = commands
 	app.Action = func(c *cli.Context) {
 		println("foo")
 	}
